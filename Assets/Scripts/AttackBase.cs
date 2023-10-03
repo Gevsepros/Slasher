1187e588ce1c167aa70dbc6f1bc27e387de4ea11
Gevsepros <mi2sha25@gmail.com>
2023-10-03 15:48:53 +0100

    refresh

---
+++ Assets/Scripts/AttackBase.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameEnums;
+
+public class AttackBase : MonoBehaviour
+{
+    public int Damage {get; private set;}
+    public int BlockDamage {get; private set;} 
+    public HitAnim HitAnims {get; private set;}
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/EnemyAi.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAi : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Animator animator;
+    Transform player;
+
+    [SerializeField] LayerMask whatIsGround, whatIsPlayer;
+
+    [SerializeField] float health;
+    [SerializeField] float chaseSpeed;
+
+    //Patroling
+    Vector3 walkPoint;
+    bool walkPointSet;
+    [SerializeField] float walkPointRange;
+    [SerializeField] float patrolSpeed;
+
+    //Attacking
+    [SerializeField] float timeBetweenAttacks;
+    bool alreadyAttacked;
+    [SerializeField] GameObject attackHitBox;
+
+    //States
+    [SerializeField] float sightRange, attackRange;
+    bool playerInSightRange, playerInAttackRange;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        //Check for sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange) Patroling();
+        else if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        else if (playerInAttackRange && playerInSightRange) AttackPlayer();
+
+        animator.SetFloat("Speed", agent.velocity.magnitude);
+    }
+
+    private void Patroling()
+    {
+        agent.speed = patrolSpeed;
+        if (!walkPointSet) SearchWalkPoint();
+        else agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //Walkpoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+    }
+    private void SearchWalkPoint()
+    {
+        //Calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = true;
+    }
+
+    private void ChasePlayer()
+    {
+        agent.speed = chaseSpeed;
+        agent.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        //Make sure enemy doesn't move
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+            ///Attack code here 
+            animator.SetTrigger("Attack");
+            ///
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        //if (health <= 0) Invoke(nameof(DestroyEnemy), 0.5f);
+    }
+}
